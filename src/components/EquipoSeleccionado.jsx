b48713c36ad8e0b78d8d4f0bbc9360fc7e955639
Daniel Armas <daniel62armas@gmail.com>
2020-04-10 14:19:02 -0500

    Equipo añadido con suplentes y titulares

--- src/components/EquipoSeleccionado.jsx
+++ src/components/EquipoSeleccionado.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Titulares from './Titulares'
+import Suplentes from './Suplentes'
 
 const EquipoSeleccionado = () => (
     <section>
--- src/store.js
+++ src/store.js
@@ -15,12 +15,94 @@ const initialState ={
         id: 3,
         nombre: "Gordis Ángeles",
         foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 4,
+        nombre: "Marco Satvrn",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 5,
+        nombre: "Rulo García",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 6,
+        nombre: "Yayo Gómez",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 7,
+        nombre: "Gonzo Ramírez",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 8,
+        nombre: "Dorilocos Rebolledo",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 9,
+        nombre: "Angelito Martínez",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 10,
+        nombre: "Dani Armas",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 11,
+        nombre: "Paul Hidalgo",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 12,
+        nombre: "Barry Nava",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
+    },
+    {
+        id: 13,
+        nombre: "Pancho Pérez",
+        foto: "https://image.flaticon.com/icons/svg/2471/2471853.svg"
     }],
     titulares: [],
     suplentes: []
 }
 
 const reducerEntrenador = (state = initialState, action)  => {
+    if (action.type === "AGREGAR_TITULAR"){
+        return {
+            ...state,
+            titulares: state.titulares.concat(action.jugador),
+            jugadores: state.jugadores.filter(j => j.id !== action.jugador.id)
+        }
+    }
+
+    if (action.type === "AGREGAR_SUPLENTE"){
+        return {
+            ...state,
+            suplentes: state.suplentes.concat(action.jugador),
+            jugadores: state.jugadores.filter(j => j.id !== action.jugador.id)
+        }
+    }
+
+    if (action.type === "QUITAR_TITULAR"){
+        return {
+            ...state,
+            titulares: state.titulares.filter(j => j.id !== action.jugador.id),
+            jugadores: state.jugadores.concat(action.jugador)
+        }
+    }
+
+    if (action.type === "QUITAR_SUPLENTE"){
+        return {
+            ...state,
+            suplentes: state.suplentes.filter(j => j.id !== action.jugador.id),
+            jugadores: state.jugadores.concat(action.jugador)
+        }
+    }
+
     return state
 }
 
